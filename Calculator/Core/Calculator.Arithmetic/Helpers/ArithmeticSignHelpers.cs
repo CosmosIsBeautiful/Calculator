4b4f756fe0e7164eb3d0774d25502602a50cf45e
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2021-03-03 20:50:29 +0500

    add Arithmetic sign helper for sign detection

---
+++ Calculator/Core/Calculator.Arithmetic/Helpers/ArithmeticSignHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Calculator.Arithmetic.Attributes;
+using Calculator.Arithmetic.Enums;
+using System;
+using System.Reflection;
+
+namespace Calculator.Arithmetic.Helpers
+{
+    public static class ArithmeticSignHelpers
+    {
+        public static char GetSign(this ArithmeticSign enumElement)
+        {
+            Type type = enumElement.GetType();
+            MemberInfo[] memInfo = type.GetMember(enumElement.ToString());
+
+            if (memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(ArithmeticSignAttribute), false);
+
+                if (attrs.Length > 0)
+                {
+                    char sign = ((ArithmeticSignAttribute)attrs[0]).Sign;
+                    return sign;
+                }
+            }
+
+            throw new NullReferenceException("The sign has no ArithmeticSign attribute");
+        }
+    }
+}
