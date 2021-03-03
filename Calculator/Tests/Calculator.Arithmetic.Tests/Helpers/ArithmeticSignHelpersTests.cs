18bbfc7270b5671ddae615c106943f4334111632
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2021-03-03 20:53:10 +0500

    add Arithmetic test project with helper check

---
+++ Calculator/Tests/Calculator.Arithmetic.Tests/Helpers/ArithmeticSignHelpersTests.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.Arithmetic.Enums;
+using Calculator.Arithmetic.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Arithmetic.Tests.Helpers
+{
+    [TestClass]
+    public class ArithmeticSignHelpersTests
+    {
+        [DataTestMethod]
+        [DataRow(ArithmeticSign.Mul, '*')]
+        [DataRow(ArithmeticSign.Del, '/')]
+        [DataRow(ArithmeticSign.Sum, '+')]
+        [DataRow(ArithmeticSign.Sub, '-')]
+        public void GetSign_CorrectSign(ArithmeticSign sign, char expectedSign)
+        {
+            //arrange
+            char actualSign = sign.GetSign();
+
+            //assert
+            Assert.AreEqual(expectedSign, actualSign, $"Actual: {actualSign} != Expected: {expectedSign}");
+        }
+
+    }
+}
