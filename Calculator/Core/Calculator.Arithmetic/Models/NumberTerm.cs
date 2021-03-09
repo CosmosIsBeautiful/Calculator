8173fec30b9ef8d003b51cb8a177dbf29d525be6
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2021-03-09 21:04:48 +0500

    add Equals for NumberTerm

--- Calculator/Core/Calculator.Arithmetic/Models/NumberTerm.cs
+++ Calculator/Core/Calculator.Arithmetic/Models/NumberTerm.cs
@@ -1,9 +1,10 @@
 ﻿using Calculator.Arithmetic.Enums;
 using Calculator.Arithmetic.Helpers;
+using System;
 
 namespace Calculator.Arithmetic.Models
 {
-    public class NumberTerm : ITerm
+    public class NumberTerm : ITerm, IEquatable<NumberTerm>
     {
         public ArithmeticSign Sign { get; set; }
 
@@ -24,6 +25,35 @@ namespace Calculator.Arithmetic.Models
         {
             this.Digit = digit;
             this.Sign = ArithmeticSignHelpers.GetArithmeticSignType(sign);
+        }
+
+        public bool Equals(NumberTerm otherNumber)
+        {
+            if (otherNumber == null)
+            {
+                return false;
+            }
+
+            return (this.Digit == otherNumber.Digit)
+                && (this.Sign == otherNumber.Sign);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || (obj is NumberTerm == false))
+            {
+                return false;
+            }
+
+            var otherNumber = obj as NumberTerm;
+
+            return (this.Digit == otherNumber.Digit)
+                && (this.Sign == otherNumber.Sign);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Digit.GetHashCode() ^ this.Sign.GetHashCode();
         }
     }
 }
