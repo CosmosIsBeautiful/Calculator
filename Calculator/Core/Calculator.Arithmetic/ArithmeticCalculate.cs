2bc6c079a18e49cad0d1c231cfaba7c6fdfae60f
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2021-03-31 10:34:47 +0500

    add Calculate inner expression

---
+++ Calculator/Core/Calculator.Arithmetic/ArithmeticCalculate.cs
@@ -0,0 +1,94 @@
+﻿using Calculator.Arithmetic.Helpers;
+using Calculator.Arithmetic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator.Arithmetic
+{
+    public interface IArithmeticCalculate
+    {
+        double Calculate(IList<ITerm> expression);
+
+        NumberTerm CalculateInnerExpression(IList<ITerm> expression);
+    }
+
+    public class ArithmeticCalculate : IArithmeticCalculate
+    {
+
+        public double Calculate(IList<ITerm> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetExpressionTermPositionNumber(IList<ITerm> expression)
+        {
+            for (int i = 0; i < expression.Count; i++)
+            {
+                if (expression[i] is ExpressionTerm)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        private IList<ITerm> GetInnerExpression(IList<ITerm> expression)
+        {
+            IList<ITerm> innerExpression = null;
+
+            for (int i = 0; i < expression.Count; i++)
+            {
+                if (expression[i] is ExpressionTerm)
+                {
+                    innerExpression = ((ExpressionTerm)expression[i]).Expression;
+                    break;
+                }
+            }
+
+            return innerExpression;
+        }
+
+        public NumberTerm CalculateInnerExpression(IList<ITerm> expression)
+        {
+            var numbers = expression.Cast<NumberTerm>().ToList();
+
+            for (int i = 0; numbers.Count != 1; i++)
+            {
+                var index = GetHighestProirityPositionNumber(numbers);
+
+                numbers[index - 1] = numbers[index - 1].MakeOperation(numbers[index]);
+                numbers.RemoveAt(index);
+            }
+            
+            return numbers[0];
+        }
+
+        private int GetHighestProirityPositionNumber(IList<NumberTerm> numberExpression)
+        {
+            int maxPriority = 0;
+            int index = 0;
+
+            if (numberExpression.Count == 2)
+            {
+                return 1;
+            }
+
+            for (int i = 1; i < numberExpression.Count; i++)
+            {
+                var currentPriority = numberExpression[i].Sign.GetPriority();
+               
+                if (currentPriority > maxPriority)
+                {
+                    maxPriority = currentPriority;
+                    index = i;
+                }
+
+            }
+
+            return index;
+        }
+    }
+}
---
+++ Calculator/Tests/Calculator.Arithmetic.Tests/ArithmeticCalculateTests.cs
@@ -0,0 +1,79 @@
+﻿using Calculator.Arithmetic.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Calculator.Arithmetic.Tests
+{
+    [TestClass]
+    public class ArithmeticCalculateTests
+    {
+        private static IArithmeticCalculate Calculate { get; set; }
+
+        [ClassInitialize]
+        public static void InitializeTests(TestContext testContext)
+        {
+            Calculate = new ArithmeticCalculate();
+        }
+
+        [TestMethod]
+        public void CalculateInnerExpression()
+        {
+            Dictionary<NumberTerm, List<ITerm>> expressions = new Dictionary<NumberTerm, List<ITerm>>
+            {
+                { new NumberTerm('+', 65), new List<ITerm>
+                    {
+                        new NumberTerm('+', 50),
+                        new NumberTerm('+', 5),
+                        new NumberTerm('*', 3),
+                    }
+                },
+                { new NumberTerm('+', 5), new List<ITerm>
+                    {
+                        new NumberTerm('+', -12),
+                        new NumberTerm('*', 7),
+                        new NumberTerm('+', 3),
+                        new NumberTerm('*', 30),
+                        new NumberTerm('-', 1),
+                    }
+                },
+                { new NumberTerm('+', -78.8), new List<ITerm>
+                    {
+                        new NumberTerm('+', -12),
+                        new NumberTerm('-', 4),
+                        new NumberTerm('*', 91),
+                        new NumberTerm('/', 5),
+                        new NumberTerm('+', 10),
+                        new NumberTerm('-', 2),
+                        new NumberTerm('*', 2),
+                    }
+                },
+                { new NumberTerm('+', 29.6), new List<ITerm>
+                    {
+                   
+                        new NumberTerm('+', 74),
+                        new NumberTerm('/', 2),
+                        new NumberTerm('/', 10),
+                        new NumberTerm('*', 8),
+                    }
+                },
+                { new NumberTerm('+', 240), new List<ITerm>
+                    {
+                        new NumberTerm('+', -60),
+                        new NumberTerm('*', -18),
+                        new NumberTerm('/', 2),
+                        new NumberTerm('-', 300),
+                    }
+                },
+            };
+
+            foreach (var item in expressions)
+            {
+                //arrange
+                var actual = Calculate.CalculateInnerExpression(item.Value);
+
+                Assert.AreEqual(item.Key, actual, $" Actual: {actual.Sign} {actual.Digit} ! = Expression: {item.Key.Sign} {item.Key.Digit}");
+            }
+        }
+    }
+}
